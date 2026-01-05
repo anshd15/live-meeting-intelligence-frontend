b2bbf110155ae018801b3b6203f1f42a804e2c13
anshd15 <anshdoshi.d16@gmail.com>
2026-01-06 03:01:37 +0530

    refactor: auth setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB35hcdG4LEtbstuG57Oi1FAh5h-uMuNgo",
-  authDomain: "live-meeting-bd35f.firebaseapp.com",
-  projectId: "live-meeting-bd35f",
-  storageBucket: "live-meeting-bd35f.firebasestorage.app",
-  messagingSenderId: "669013656160",
-  appId: "1:669013656160:web:fbc616492c68f9a71cf905"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
