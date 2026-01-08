f226f48213b3a8503c01eea2daf75b58f893cdd6
anshd15 <anshdoshi.d16@gmail.com>
2026-01-09 00:54:01 +0530

    fix: eliminating out JoinRoom component in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Hero from "./Hero";
-import JoinRoom from "./JoinRoom";
+// import JoinRoom from "./JoinRoom";
 import Features from "./Features";
 import Developer from "./Developer";
 import Footer from "./Footer";
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-black text-white overflow-hidden">
       <Hero />
-      <JoinRoom />
+      {/* <JoinRoom /> */}
       <Features />
       <Developer />
       <Footer />
